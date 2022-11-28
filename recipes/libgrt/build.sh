22b0b0cdfb93e301e1187ba1883eb008d699976a
Andreas Kirsch <blackhc@gmail.com>
2022-11-28 22:46:29 +0000

    Add a recipe for building libgrt right off my master branch (for now)

---
+++ recipes/libgrt/build.sh
@@ -0,0 +1,15 @@
+
+cd build
+mkdir release
+cd release
+cmake ${CMAKE_ARGS} -G "Ninja"                                            \
+    -DCMAKE_BUILD_TYPE="Release"                                          \
+    -DCMAKE_PREFIX_PATH=${PREFIX}                                         \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}                                      \
+    -DCMAKE_INSTALL_LIBDIR="lib"                                          \
+    -DPYTHON_PACKAGES_PATH=${SP_DIR}                                      \
+    -DPYTHON_EXECUTABLE=${PYTHON}                                         \
+    -DBUILD_EXAMPLES=OFF                                                  \
+    ..
+
+ninja install -v -j${CPU_COUNT}
